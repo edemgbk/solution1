dc056805dcdc2a2bc5ecdf06d279796f93f67c5c
edem kofi gbeku <edemgbk@edems-MacBook-Pro.local>
2020-09-04 16:10:33 +0000

    foruth commit

--- src/Home.js
+++ src/Home.js
@@ -21,7 +21,8 @@ const Home = () => (
       No Time to Die is a forthcoming spy film and the twenty-fifth instalment in the James Bond film series produced by Eon Productions. The film features Daniel Craig in his fifth and final outing as the fictional MI6 agent James Bond
       </p>
       <h6>No time To Die</h6>
-      <div class="s3bubble" data-code="qg557q"></div>
+      <div class="s3bubble" data-code="qg557q" style="width:100%; height:600px;"></div>
+
     </div>
   );
   
